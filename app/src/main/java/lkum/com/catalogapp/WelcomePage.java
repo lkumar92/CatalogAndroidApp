9e11cc044c8009b49a2e5ea0671184af5515e059
lkumar11 <lkumar11@sapient.com>
2018-02-28 09:41:50 +0530

    Main page changes done.

--- app/src/main/java/lkum/com/catalogapp/WelcomePage.java
+++ app/src/main/java/lkum/com/catalogapp/WelcomePage.java
@@ -1,7 +1,10 @@
 package lkum.com.catalogapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class WelcomePage extends AppCompatActivity {
 
@@ -9,5 +12,15 @@ public class WelcomePage extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
+    }
+
+    public void toMainPage(View v) {
+        // does something very interesting
+        Toast toast = Toast.makeText(this, "Redirecting to Main Page", Toast.LENGTH_SHORT);
+        toast.show();
+        Intent myIntent = new Intent(WelcomePage.this,
+                MainPage.class);
+        startActivity(myIntent);
+        //startActivity(myIntent);
     }
 }
